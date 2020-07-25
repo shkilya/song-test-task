5450b285b39cd9fc02c32133440db05e057f9f3c
shkilya <shkilya1994@gmail.com>
2020-07-25 13:12:06 +0300

    fix migration

--- migrations/Version20200725075938.php
+++ migrations/Version20200725075938.php
@@ -20,10 +20,6 @@ final class Version20200725075938 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment DROP CONSTRAINT fk_9474526c604b8382');
-        $this->addSql('DROP SEQUENCE comment_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE conference_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE hazardous_asteroids_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE song_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE song (id INT NOT NULL, name VARCHAR(255) NOT NULL, singer VARCHAR(255) NOT NULL, year INT NOT NULL, duration INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE hazardous_asteroids');
@@ -36,15 +32,6 @@ final class Version20200725075938 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE song_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE comment_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE conference_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE hazardous_asteroids_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE hazardous_asteroids (id INT NOT NULL, date DATE NOT NULL, neo_reference_id INT NOT NULL, name VARCHAR(255) NOT NULL, kilometers_per_hour DOUBLE PRECISION NOT NULL, is_potentially_hazardous_asteroid BOOLEAN NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_b60605d91de20a4c ON hazardous_asteroids (neo_reference_id)');
-        $this->addSql('CREATE TABLE conference (id INT NOT NULL, city VARCHAR(255) NOT NULL, year VARCHAR(4) NOT NULL, is_international BOOLEAN NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE comment (id INT NOT NULL, conference_id INT DEFAULT NULL, author VARCHAR(255) NOT NULL, text TEXT NOT NULL, email VARCHAR(255) NOT NULL, photo_filename VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX idx_9474526c604b8382 ON comment (conference_id)');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT fk_9474526c604b8382 FOREIGN KEY (conference_id) REFERENCES conference (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('DROP TABLE song');
     }
 }
